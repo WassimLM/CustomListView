2acf414cce4b139cedd371519067703507372179
WassimLM <wassimlajem12@gmail.com>
2019-10-09 13:25:03 +0100

    Initial commit

---
+++ app/src/main/java/com/firasshaymasihem/customlistview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.firasshaymasihem.customlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
